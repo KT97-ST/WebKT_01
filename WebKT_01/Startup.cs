638ed03219dc21289812764376305446347b3d66
kthanhktst <kimthanhktst@gmail.com>
2023-02-13 16:35:54 +0700

    #2

--- WebKT_01/Startup.cs
+++ WebKT_01/Startup.cs
@@ -8,6 +8,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebKT_01.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace WebKT_01
 {
@@ -24,6 +27,9 @@ namespace WebKT_01
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            var connectionString = Configuration.GetConnectionString("KTCoreProject01_DB");
+            services.AddDbContext<KTCoreProject01_DBContext>(options => options.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,11 +68,7 @@ namespace WebKT_01
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-                endpoints.MapControllerRoute(
-                name: "Home",
-                pattern: "{Home}",
-                new { controller = "Home", action = "Index" });
+       
             });
         }
     }
